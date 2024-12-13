ae0c0285ab08062d199b2c857630318f093bb9ee
Muhammad Atif Iqbal <68033692+atifwattoo@users.noreply.github.com>
2024-12-14 00:02:24 +0500

    PriceCandlestickChart integration done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import Symbols from "./components/Symbols";
+import PriceCandlestickChart from "./components/charts/PriceCandlestickChart";
 
 function App() {
   // const [symbols, setSymbols] = useState([]);
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to our Cypto App</h1>
+      <PriceCandlestickChart />
       <Symbols />
 
     </div>
